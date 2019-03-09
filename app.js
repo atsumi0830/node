5b2e335c1212b5d45493be969fdd18477cb99ccc
Kentaro-Atsumi <monarchs0830@gmail.com>
2019-03-09 14:16:25 +0900

    yargs インストール

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ console.log('app.js 作業開始');
 
 const fs = require('fs');
 const os = require('os');
+const yargs = require('yargs');
+const argv = yargs.argv;
+
 
 let user = os.userInfo();
 
@@ -9,9 +12,23 @@ let notes = require('./notes.js');
 
 let result = notes.addNote();
 
+let command = process.argv[2];
+console.log("コマンド:", command);
 
-fs.appendFile('greeting.txt', 'hello! ' + user.username + 'さんは' + notes.age + '歳です。', function (err) {
-    if(err) {
-        console.log(err);
-    }
-});
+console.log(argv);
+
+// fs.appendFile('greeting.txt', 'hello! ' + user.username + 'さんは' + notes.age + '歳です。', function (err) {
+//     if(err) {
+//         console.log(err);
+//     }
+// });
+
+if(command === 'add') {
+    console.log('メモを追加');
+} else if(command === 'list') {
+    console.log('メモを一覧表示');
+} else if(command === 'read') {
+    console.log('メモを個別表示');
+} else if(command === 'remove') {
+    console.log('メモを削除');
+}
